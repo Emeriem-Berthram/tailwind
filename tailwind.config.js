f0601a4632a4cb3147aeb6c720f696579f3d7641
Emeriem-Berthram <emeriemberthram@gmail.com>
2024-08-10 22:22:19 -0700

    remove node_modules directory

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src//*.{html,js}"],
+  theme: {
+    extend: {colors: {
+        Green: "hsl(75, 94%, 57%)",
+        White: "hsl(0, 0%, 100%)",
+        Grey700: "hsl(0, 0%, 20%)",
+       Grey800: "hsl(0, 0%, 12%)", 
+       Grey900: "hsl(0, 0%, 8%)",
+      },},
+  },
+  plugins: [],
+}
+
